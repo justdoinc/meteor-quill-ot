4c8a44a0ae81afdb6d9edea4687851f93aa3f887
Joshua Ohlman <codewriterohs@gmail.com>
2017-03-01 15:47:57 -0600

    Add useful inspect and toString to quill delta

--- quill-delta/delta.js
+++ quill-delta/delta.js
@@ -4,4 +4,14 @@
 // applications. See delta-tests.js for an example of importing.
 import Delta from 'quill-delta';
 
+Delta.prototype.inspect = function () {
+  return "[Delta " + this.ops.map(function (op) {
+    if (op.insert != null) return "+++" + op.insert;
+    if (op.retain != null) return "..." + op.retain;
+    if (op.delete != null) return "---" + op.delete;
+  }).join("") + "]"
+}
+
+Delta.prototype.toString = Delta.prototype.inspect
+
 export { Delta };
