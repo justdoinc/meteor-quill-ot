e55773f2095fc5afce98696b2d13a9c036ee535c
Joshua Ohlman <codewriterohs@gmail.com>
2017-03-09 14:26:19 -0600

    Update quill-delta readme and remove unnecessary comments

--- quill-delta/README.md
+++ quill-delta/README.md
@@ -4,3 +4,8 @@ Meteor Delta
 Meteor wrapper package for the quilljs/ot-types delta format.
 
 TODO: Make Delta an e-json compatible type
+
+Testing
+====
+
+`meteor test-packages ./`
--- quill-delta/delta.js
+++ quill-delta/delta.js
@@ -1,7 +1,3 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See delta-tests.js for an example of importing.
 import Delta from 'quill-delta';
 
 Delta.prototype.inspect = function () {
